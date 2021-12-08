dfe5201ea1ec2c9c6fb3800dfe3d663b4304e446
Amish Choudhary <83035765+Amish07092001@users.noreply.github.com>
2021-12-09 01:29:31 +0530

    Update PathfindingVisualiser.java

--- PathfindingVisualiser.java
+++ PathfindingVisualiser.java
@@ -27,9 +27,7 @@ import javax.swing.JComboBox;
 
 public class PathFinding {
 	
-	//FRAME
 	JFrame frame;
-	//GENERAL VARIABLES
 	private int cells = 20;
 	private int delay = 30;
 	private double dense = .5;
@@ -46,19 +44,14 @@ public class PathFinding {
 	private final int HEIGHT = 650;
 	private final int MSIZE = 600;
 	private int CSIZE = MSIZE/cells;
-	//UTIL ARRAYS
 	private String[] algorithms = {"Dijkstra","A*"};
 	private String[] tools = {"Start","Finish","Wall", "Eraser"};
-	//BOOLEANS
 	private boolean solving = false;
-	//UTIL
 	Node[][] map;
 	Algorithm Alg = new Algorithm();
 	Random r = new Random();
-	//SLIDERS
 	JSlider size = new JSlider(1,5,2);
 	JSlider speed = new JSlider(0,500,delay);
-
 	JLabel algL = new JLabel("Algorithms");
 	JLabel toolL = new JLabel("Toolbox");
 	JLabel sizeL = new JLabel("Size:");
@@ -67,65 +60,60 @@ public class PathFinding {
 	JLabel msL = new JLabel(delay+"ms");
 	JLabel checkL = new JLabel("Checks: "+checks);
 	JLabel lengthL = new JLabel("Path Length: "+length);
-	//BUTTONS
 	JButton searchB = new JButton("Start Search");
 	JButton resetB = new JButton("Reset");
 	JButton clearMapB = new JButton("Clear Map");
 	JButton creditB = new JButton("Credit");
-	//DROP DOWN
 	JComboBox algorithmsBx = new JComboBox(algorithms);
 	JComboBox toolBx = new JComboBox(tools);
-	//PANELS
 	JPanel toolP = new JPanel();
-	//CANVAS
 	Map canvas;
-	//BORDER
 	Border loweredetched = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
 
 	public static void main(String[] args) {
-		//MAIN METHOD
+		
 		new PathFinding();
 	}
 
-	public PathFinding() {	//CONSTRUCTOR
+	public PathFinding() {
 		clearMap();
 		initialize();
 	}
 	
 
 	
-	public void clearMap() {	//CLEAR MAP
-		finishx = -1;	//RESET THE START AND FINISH
+	public void clearMap() {	
+		finishx = -1;	
 		finishy = -1;
 		startx = -1;
 		starty = -1;
-		map = new Node[cells][cells];	//CREATE NEW MAP OF NODES
+		map = new Node[cells][cells];	
 		for(int x = 0; x < cells; x++) {
 			for(int y = 0; y < cells; y++) {
-				map[x][y] = new Node(3,x,y);	//SET ALL NODES TO EMPTY
+				map[x][y] = new Node(3,x,y);	
 			}
 		}
-		reset();	//RESET SOME VARIABLES
+		reset();	
 	}
 	
-	public void resetMap() {	//RESET MAP
+	public void resetMap() {	
 		for(int x = 0; x < cells; x++) {
 			for(int y = 0; y < cells; y++) {
 				Node current = map[x][y];
-				if(current.getType() == 4 || current.getType() == 5)	//CHECK TO SEE IF CURRENT NODE IS EITHER CHECKED OR FINAL PATH
-					map[x][y] = new Node(3,x,y);	//RESET IT TO AN EMPTY NODE
+				if(current.getType() == 4 || current.getType() == 5)	
+					map[x][y] = new Node(3,x,y);
 			}
 		}
-		if(startx > -1 && starty > -1) {	//RESET THE START AND FINISH
+		if(startx > -1 && starty > -1) {	
 			map[startx][starty] = new Node(0,startx,starty);
 			map[startx][starty].setHops(0);
 		}
 		if(finishx > -1 && finishy > -1)
 			map[finishx][finishy] = new Node(1,finishx,finishy);
-		reset();	//RESET SOME VARIABLES
+		reset();	
 	}
 
-	private void initialize() {	//INITIALIZE THE GUI ELEMENTS
+	private void initialize() {
 		frame = new JFrame();
 		frame.setVisible(true);
 		frame.setResizable(false);
@@ -209,7 +197,7 @@ public class PathFinding {
 		canvas.setBounds(230, 10, MSIZE+1, MSIZE+1);
 		frame.getContentPane().add(canvas);
 		
-		searchB.addActionListener(new ActionListener() {		//ACTION LISTENERS
+		searchB.addActionListener(new ActionListener() {		
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				reset();
@@ -272,10 +260,10 @@ public class PathFinding {
 			}
 		});
 		
-		startSearch();	//START STATE
+		startSearch();	
 	}
 	
-	public void startSearch() {	//START STATE
+	public void startSearch() {	
 		if(solving) {
 			switch(curAlg) {
 				case 0:
@@ -286,10 +274,10 @@ public class PathFinding {
 					break;
 			}
 		}
-		pause();	//PAUSE STATE
+		pause();	
 	}
 	
-	public void pause() {	//PAUSE STATE
+	public void pause() {	
 		int i = 0;
 		while(!solving) {
 			i++;
@@ -299,10 +287,10 @@ public class PathFinding {
 				Thread.sleep(1);
 			} catch(Exception e) {}
 		}
-		startSearch();	//START STATE
+		startSearch();	
 	}
 	
-	public void Update() {	//UPDATE ELEMENTS OF THE GUI
+	public void Update() {	
 		density = (cells*cells)*dense;
 		CSIZE = MSIZE/cells;
 		canvas.repaint();
@@ -312,28 +300,27 @@ public class PathFinding {
 		checkL.setText("Checks: "+checks);
 	}
 	
-	public void reset() {	//RESET METHOD
+	public void reset() {	
 		solving = false;
 		length = 0;
 		checks = 0;
 	}
 	
-	public void delay() {	//DELAY METHOD
+	public void delay() {	
 		try {
 			Thread.sleep(delay);
 		} catch(Exception e) {}
 	}
 	
-	class Map extends JPanel implements MouseListener, MouseMotionListener{	//MAP CLASS
-		
+	class Map extends JPanel implements MouseListener, MouseMotionListener{	
 		public Map() {
 			addMouseListener(this);
 			addMouseMotionListener(this);
 		}
 		
-		public void paintComponent(Graphics g) {	//REPAINT
+		public void paintComponent(Graphics g) {	
 			super.paintComponent(g);
-			for(int x = 0; x < cells; x++) {	//PAINT EACH NODE IN THE GRID
+			for(int x = 0; x < cells; x++) {	
 				for(int y = 0; y < cells; y++) {
 					switch(map[x][y].getType()) {
 						case 0:
@@ -358,13 +345,7 @@ public class PathFinding {
 					g.fillRect(x*CSIZE,y*CSIZE,CSIZE,CSIZE);
 					g.setColor(Color.BLACK);
 					g.drawRect(x*CSIZE,y*CSIZE,CSIZE,CSIZE);
-					//DEBUG STUFF
-					/*
-					if(curAlg == 1)
-						g.drawString(map[x][y].getHops()+"/"+map[x][y].getEuclidDist(), (x*CSIZE)+(CSIZE/2)-10, (y*CSIZE)+(CSIZE/2));
-					else 
-						g.drawString(""+map[x][y].getHops(), (x*CSIZE)+(CSIZE/2), (y*CSIZE)+(CSIZE/2));
-					*/
+					
 				}
 			}
 		}
@@ -395,32 +376,32 @@ public class PathFinding {
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			resetMap();	//RESET THE MAP WHENEVER CLICKED
+			resetMap();	
 			try {
-				int x = e.getX()/CSIZE;	//GET THE X AND Y OF THE MOUSE CLICK IN RELATION TO THE SIZE OF THE GRID
+				int x = e.getX()/CSIZE;	
 				int y = e.getY()/CSIZE;
 				Node current = map[x][y];
 				switch(tool ) {
-					case 0: {	//START NODE
-						if(current.getType()!=2) {	//IF NOT WALL
-							if(startx > -1 && starty > -1) {	//IF START EXISTS SET IT TO EMPTY
+					case 0: {	
+						if(current.getType()!=2) {	
+							if(startx > -1 && starty > -1) {	
 								map[startx][starty].setType(3);
 								map[startx][starty].setHops(-1);
 							}
 							current.setHops(0);
-							startx = x;	//SET THE START X AND Y
+							startx = x;	
 							starty = y;
-							current.setType(0);	//SET THE NODE CLICKED TO BE START
+							current.setType(0);	
 						}
 						break;
 					}
-					case 1: {//FINISH NODE
-						if(current.getType()!=2) {	//IF NOT WALL
-							if(finishx > -1 && finishy > -1)	//IF FINISH EXISTS SET IT TO EMPTY
+					case 1: {
+						if(current.getType()!=2) {	
+							if(finishx > -1 && finishy > -1)	
 								map[finishx][finishy].setType(3);
-							finishx = x;	//SET THE FINISH X AND Y
+							finishx = x;	
 							finishy = y;
-							current.setType(1);	//SET THE NODE CLICKED TO BE FINISH
+							current.setType(1);	
 						}
 						break;
 					}
@@ -430,48 +411,36 @@ public class PathFinding {
 						break;
 				}
 				Update();
-			} catch(Exception z) {}	//EXCEPTION HANDLER
+			} catch(Exception z) {}	
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {}
 	}
 	
-	class Algorithm {	//ALGORITHM CLASS
-		
-		//DIJKSTRA WORKS BY PROPAGATING OUTWARDS UNTIL IT FINDS THE FINISH AND THEN WORKING ITS WAY BACK TO GET THE PATH
-		//IT USES A PRIORITY QUE TO KEEP TRACK OF NODES THAT IT NEEDS TO EXPLORE
-		//EACH NODE IN THE PRIORITY QUE IS EXPLORED AND ALL OF ITS NEIGHBORS ARE ADDED TO THE QUE
-		//ONCE A NODE IS EXLPORED IT IS DELETED FROM THE QUE
-		//AN ARRAYLIST IS USED TO REPRESENT THE PRIORITY QUE
-		//A SEPERATE ARRAYLIST IS RETURNED FROM A METHOD THAT EXPLORES A NODES NEIGHBORS
-		//THIS ARRAYLIST CONTAINS ALL THE NODES THAT WERE EXPLORED, IT IS THEN ADDED TO THE QUE
-		//A HOPS VARIABLE IN EACH NODE REPRESENTS THE NUMBER OF NODES TRAVELED FROM THE START
+	class Algorithm {	
 		public void Dijkstra() {
-			ArrayList<Node> priority = new ArrayList<Node>();	//CREATE A PRIORITY QUE
-			priority.add(map[startx][starty]);	//ADD THE START TO THE QUE
+			ArrayList<Node> priority = new ArrayList<Node>();	
+			priority.add(map[startx][starty]);	
 			while(solving) {
-				if(priority.size() <= 0) {	//IF THE QUE IS 0 THEN NO PATH CAN BE FOUND
+				if(priority.size() <= 0) {	
 					solving = false;
 					break;
 				}
-				int hops = priority.get(0).getHops()+1;	//INCREMENT THE HOPS VARIABLE
-				ArrayList<Node> explored = exploreNeighbors(priority.get(0), hops);	//CREATE AN ARRAYLIST OF NODES THAT WERE EXPLORED
+				int hops = priority.get(0).getHops()+1;	
+				ArrayList<Node> explored = exploreNeighbors(priority.get(0), hops);	
 				if(explored.size() > 0) {
-					priority.remove(0);	//REMOVE THE NODE FROM THE QUE
-					priority.addAll(explored);	//ADD ALL THE NEW NODES TO THE QUE
+					priority.remove(0);	
+					priority.addAll(explored);	
 					Update();
 					delay();
-				} else {	//IF NO NODES WERE EXPLORED THEN JUST REMOVE THE NODE FROM THE QUE
+				} else {	
 					priority.remove(0);
 				}
 			}
 		}
 		
-		//A STAR WORKS ESSENTIALLY THE SAME AS DIJKSTRA CREATING A PRIORITY QUE AND PROPAGATING OUTWARDS UNTIL IT FINDS THE END
-		//HOWEVER ASTAR BUILDS IN A HEURISTIC OF DISTANCE FROM ANY NODE TO THE FINISH
-		//THIS MEANS THAT NODES THAT ARE CLOSER TO THE FINISH WILL BE EXPLORED FIRST
-		//THIS HEURISTIC IS BUILT IN BY SORTING THE QUE ACCORDING TO HOPS PLUS DISTANCE UNTIL THE FINISH
+		
 		public void AStar() {
 			ArrayList<Node> priority = new ArrayList<Node>();
 			priority.add(map[startx][starty]);
@@ -490,11 +459,11 @@ public class PathFinding {
 				} else {
 					priority.remove(0);
 				}
-				sortQue(priority);	//SORT THE PRIORITY QUE
+				sortQue(priority);	
 			}
 		}
 		
-		public ArrayList<Node> sortQue(ArrayList<Node> sort) {	//SORT PRIORITY QUE
+		public ArrayList<Node> sortQue(ArrayList<Node> sort) {	
 			int c = 0;
 			while(c < sort.size()) {
 				int sm = c;
@@ -512,17 +481,17 @@ public class PathFinding {
 			return sort;
 		}
 		
-		public ArrayList<Node> exploreNeighbors(Node current, int hops) {	//EXPLORE NEIGHBORS
-			ArrayList<Node> explored = new ArrayList<Node>();	//LIST OF NODES THAT HAVE BEEN EXPLORED
+		public ArrayList<Node> exploreNeighbors(Node current, int hops) {
+			ArrayList<Node> explored = new ArrayList<Node>();	
 			for(int a = -1; a <= 1; a++) {
 				for(int b = -1; b <= 1; b++) {
 					int xbound = current.getX()+a;
 					int ybound = current.getY()+b;
-					if((xbound > -1 && xbound < cells) && (ybound > -1 && ybound < cells)) {	//MAKES SURE THE NODE IS NOT OUTSIDE THE GRID
+					if((xbound > -1 && xbound < cells) && (ybound > -1 && ybound < cells)) {	
 						Node neighbor = map[xbound][ybound];
-						if((neighbor.getHops()==-1 || neighbor.getHops() > hops) && neighbor.getType()!=2) {	//CHECKS IF THE NODE IS NOT A WALL AND THAT IT HAS NOT BEEN EXPLORED
-							explore(neighbor, current.getX(), current.getY(), hops);	//EXPLORE THE NODE
-							explored.add(neighbor);	//ADD THE NODE TO THE LIST
+						if((neighbor.getHops()==-1 || neighbor.getHops() > hops) && neighbor.getType()!=2) {	
+							explore(neighbor, current.getX(), current.getY(), hops);	
+							explored.add(neighbor);	
 						}
 					}
 				}
@@ -530,20 +499,20 @@ public class PathFinding {
 			return explored;
 		}
 		
-		public void explore(Node current, int lastx, int lasty, int hops) {	//EXPLORE A NODE
-			if(current.getType()!=0 && current.getType() != 1)	//CHECK THAT THE NODE IS NOT THE START OR FINISH
-				current.setType(4);	//SET IT TO EXPLORED
-			current.setLastNode(lastx, lasty);	//KEEP TRACK OF THE NODE THAT THIS NODE IS EXPLORED FROM
-			current.setHops(hops);	//SET THE HOPS FROM THE START
+		public void explore(Node current, int lastx, int lasty, int hops) {	
+			if(current.getType()!=0 && current.getType() != 1)	
+				current.setType(4);	
+			current.setLastNode(lastx, lasty);	
+			current.setHops(hops);	
 			checks++;
-			if(current.getType() == 1) {	//IF THE NODE IS THE FINISH THEN BACKTRACK TO GET THE PATH
+			if(current.getType() == 1) {	
 				backtrack(current.getLastX(), current.getLastY(),hops);
 			}
 		}
 		
-		public void backtrack(int lx, int ly, int hops) {	//BACKTRACK
+		public void backtrack(int lx, int ly, int hops) {	
 			length = hops;
-			while(hops > 1) {	//BACKTRACK FROM THE END OF THE PATH TO THE START
+			while(hops > 1) {	
 				Node current = map[lx][ly];
 				current.setType(5);
 				lx = current.getLastX();
@@ -565,28 +534,28 @@ public class PathFinding {
 		private int lastY;
 		private double dToEnd = 0;
 	
-		public Node(int type, int x, int y) {	//CONSTRUCTOR
+		public Node(int type, int x, int y) {	
 			cellType = type;
 			this.x = x;
 			this.y = y;
 			hops = -1;
 		}
 		
-		public double getEuclidDist() {		//CALCULATES THE EUCLIDIAN DISTANCE TO THE FINISH NODE
+		public double getEuclidDist() {		
 			int xdif = Math.abs(x-finishx);
 			int ydif = Math.abs(y-finishy);
 			dToEnd = Math.sqrt((xdif*xdif)+(ydif*ydif));
 			return dToEnd;
 		}
 		
-		public int getX() {return x;}		//GET METHODS
+		public int getX() {return x;}		
 		public int getY() {return y;}
 		public int getLastX() {return lastX;}
 		public int getLastY() {return lastY;}
 		public int getType() {return cellType;}
 		public int getHops() {return hops;}
 		
-		public void setType(int type) {cellType = type;}		//SET METHODS
+		public void setType(int type) {cellType = type;}		
 		public void setLastNode(int x, int y) {lastX = x; lastY = y;}
 		public void setHops(int hops) {this.hops = hops;}
 	}
